77b03e271d2202031fa1e5eda71b1eff5547c0e7
cyberninjas <cyberninjas@debian8x64.cyberninjas.com>
2016-10-24 13:13:48 -0400

    make actually produces a binary -- not that it does anything

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "string.h"
+
+#include "jwt.h"
+#include "cryptopp/dsa.h"
+#include "cryptopp/osrng.h"
+
+using namespace std;
+using namespace jwtcpp;
+using namespace CryptoPP;
+
+int main(){
+ cout << "lol" << endl;
+ return 0;
+}
